6ebc5acb97a4e64255dc187a78ad3e1434ef727e
Ricardo Palacios Melo <palaciosr@uabc.edu.mx>
2024-06-19 12:56:24 -0700

    Primer Commit estacionamiento

---
+++ src/main/java/com/app/estacionamiento/controllers/AutoController.java
@@ -0,0 +1,18 @@
+package com.app.estacionamiento.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/inicio")
+public class AutoController {
+	
+	@GetMapping("/listar")
+	public String listarAutos(Model model) {
+		return "/inicio/listar";
+		
+	}
+
+}
