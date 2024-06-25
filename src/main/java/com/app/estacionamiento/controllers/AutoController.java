844479397f465e4d16724421f9ab397e81d53fce
Ricardo Palacios Melo <palaciosr@uabc.edu.mx>
2024-06-25 13:06:57 -0700

    Modal y form

--- src/main/java/com/app/estacionamiento/controllers/AutoController.java
+++ src/main/java/com/app/estacionamiento/controllers/AutoController.java
@@ -1,18 +1,24 @@
 package com.app.estacionamiento.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.app.estacionamiento.service.AutoService;
+
 @Controller
 @RequestMapping("/inicio")
 public class AutoController {
 	
+	@Autowired
+	AutoService autoService;
+	
 	@GetMapping("/listar")
 	public String listarAutos(Model model) {
-		model.addAttribute("titulo","Estracionamiento las Olas");
-		return "/inicio/listar";
+		model.addAttribute("titulo","Estacionamiento las Olas");
+		return "/inicio/inicio";
 		
 	}
 }
